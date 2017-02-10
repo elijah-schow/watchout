2622e49108d219221703823c0b5dacb1f1b5cf13
elijah-schow <elijah.schow@gmail.com>
2017-02-10 15:52:37 -0600

    Adjust animation timing

--- client/watchout.js
+++ client/watchout.js
@@ -1,14 +1,24 @@
 var update = function(items) {
-  // Enemies
+
+  // Randomize enemy positions
+  items.forEach(function(value, index, collection){
+    //Randomize enemy positions
+    collection[index].x = Math.random() * 600;
+    collection[index].y = Math.random() * 600;
+  });
+
+  // Make a slection
   var selection = d3.select('.board')
     .selectAll('circle')
     .data(items);
 
+  // Update enemy positions
   selection.transition()
-    .duration(1000)
+    .duration(time)
     .attr('cx', d => d.x)
     .attr('cy', d => d.y);
 
+  // Add new enemies to the board
   selection
     .enter()
     .append('circle')
@@ -16,30 +26,27 @@ var update = function(items) {
     .attr('cx', d => d.x)
     .attr('cy', d => d.y)
     .attr('r', '15')
-    .attr('fill', 'black');
+    .attr('fill', 'black')
+    .attr('opacity', 0)
+    .transition()
+    .duration(time / 2)
+    .attr('opacity', 1);
 
-  // Remove enemies
+  // Remove non-existent enemies
   selection
     .exit()
     .remove();
 
 };
 
-var data = [
-    { 'id': 0, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
-    { 'id': 1, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
-    { 'id': 2, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
-    { 'id': 3, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
-    { 'id': 4, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
-    { 'id': 5, 'x' : Math.random() * 600, 'y' : Math.random() * 600}
-];
+const time = 1000;
+var data = [];
+var n = 20;
+for (var i = 0; i < n; i++) {
+  data.push( { 'id' : i} );
+}
 
-setInterval(function(){
-  console.log('update');
-  data.forEach(function(value, index, collection){
-    //Randomize enemy positions
-    collection[index].x = Math.random() * 600;
-    collection[index].y = Math.random() * 600;
-  });
+update(data);
+setInterval(function() {  
   update(data);
-}, 1000);
+}, time);
