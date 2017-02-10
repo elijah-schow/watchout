3b1c5f5646ad24bb6c5fa5ef5af0dbea5f3e2cc9
elijah-schow <elijah.schow@gmail.com>
2017-02-10 15:43:59 -0600

    Implement moving enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,45 @@
-// start slingin' some d3 here.
+var update = function(items) {
+  // Enemies
+  var selection = d3.select('.board')
+    .selectAll('circle')
+    .data(items);
+
+  selection.transition()
+    .duration(1000)
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y);
+
+  selection
+    .enter()
+    .append('circle')
+    .classed('enemy', true)
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
+    .attr('r', '15')
+    .attr('fill', 'black');
+
+  // Remove enemies
+  selection
+    .exit()
+    .remove();
+
+};
+
+var data = [
+    { 'id': 0, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
+    { 'id': 1, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
+    { 'id': 2, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
+    { 'id': 3, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
+    { 'id': 4, 'x' : Math.random() * 600, 'y' : Math.random() * 600},
+    { 'id': 5, 'x' : Math.random() * 600, 'y' : Math.random() * 600}
+];
+
+setInterval(function(){
+  console.log('update');
+  data.forEach(function(value, index, collection){
+    //Randomize enemy positions
+    collection[index].x = Math.random() * 600;
+    collection[index].y = Math.random() * 600;
+  });
+  update(data);
+}, 1000);
